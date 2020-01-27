516038b7512adc52b9e993f09cd8a8a2e0901393
NahidHasanAshik <nahidhasanashik99@yahoo.com>
2020-01-28 01:09:02 +0600

    useing c++ & math basic Grandi's series 1-1+1-1+1-1+1-1...=0   && 1+(-1+1)+(-1+1)+(-1+1)....=1

---
+++ URI Online Judge | 1858.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+  int N,j;
+
+  cin>>N;
+
+  for(int i=0;i<N;i++)
+  {
+
+    cin>>j;
+
+    if(j%2==0) cout<<0<<endl;
+
+    else cout<<1<<endl;
+
+  }
+
+return 0;
+}
